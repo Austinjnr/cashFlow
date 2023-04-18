7d6bcbc051238d5428dd48674392a39cd037d543
Moses <ochachamoses443@gmail.com>
2023-04-18 12:27:54 +0300

    checkect

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_17_092731) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_18_092451) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "email"
-    t.string "name"
+    t.string "username"
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
