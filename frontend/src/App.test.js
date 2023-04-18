1b6248a4634b4611bacc1402823f349e9ec82b74
Austinjnr <Austinobonyo@gmail.com>
2023-04-18 10:27:33 +0300

    folder structure

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,10 +1,23 @@
-const { add, subtract } =  require('./cashflow');
+import { render, screen, cleanup } from '@testing-library/react';
+import Login from './Components/register/Login';
+import Navigation from './Components/userNavbar/Navigation';
 
-describe('cashflow.js tests', () =>{
-    it ('should test add', () =>{
-        expect(add(2, 2)).toBe(4);
-    })
-    it ('should test subtract', () =>{
-        expect(subtract(2, 2)).toBe(0);
-    })
+afterEach(() => {
+    cleanup();
+})
+
+test('should render Login component', () =>{
+    render(<Login />);
+    const LoginElement = screen.getByTestId('login-1');
+    expect(LoginElement).toBeInTheDocument();
+    expect(LoginElement).toHaveTextContent('Welcome Back')
+    // expect(LoginElement).toContainHTML('<input type="email" id="inputEmail1" placeholder="youremail@gmail.com" />')
+    // expect(LoginElement).toContainHTML('<Button type="submit">Login</Button>')
+})
+test('should render Naviagtion component', () =>{
+    render(<Navigation />);
+    const NavigationElement = screen.getByTestId('Navigation-1');
+    expect(NavigationElement).toBeInTheDocument();
+    expect(NavigationElement).toContainHTML('<Siwtch>')
+    expect(NavigationElement).toContainHTML('<Route />')
 })
--- frontend/src/Components/userNavbar/Profile.js
+++ frontend/src/Components/userNavbar/Profile.js
@@ -1,9 +1,9 @@
 const Profile = () => {
-    return (  
+    return ( 
         <section>
-            <h2>Profile page</h2>
+            
         </section>
-    );
+     );
 }
  
 export default Profile;
