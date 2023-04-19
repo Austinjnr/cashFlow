2c696c5f3a9e36df0066d5a07b54b20b0189c5c5
RoyZiO <nziokinroy@gmail.com>
2023-04-19 15:20:03 +0300

    updated wallets and beneficiaries

--- backend/app/controllers/wallets_controller.rb
+++ backend/app/controllers/wallets_controller.rb
@@ -5,12 +5,12 @@ class WalletsController < ApplicationController
   def index
     @wallets = Wallet.all
 
-    render json: @wallets
+    render json: @wallets, status: :ok
   end
 
   # GET /wallets/1
   def show
-    render json: @wallet
+    render json: @wallet, status: :ok
   end
 
   # POST /wallets
