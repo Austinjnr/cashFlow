2037e83fd38c665b1190e31d9c0bacddf3b6466b
Moses <ochachamoses443@gmail.com>
2023-04-21 16:45:45 +0300

    deploy using postgres setup

--- backend/app/models/account.rb
+++ backend/app/models/account.rb
@@ -1,4 +1,5 @@
 require 'twilio-ruby'
+require 'dotenv/load'
 
 class Account < ApplicationRecord
   after_create :send_welcome_sms
@@ -9,26 +10,16 @@ class Account < ApplicationRecord
 
   def send_welcome_sms
     puts "Sending welcome SMS..."
-    account_sid = 'AC71fd87e5e3089d4d6a08fffe87c60bb0'
-    auth_token = 'a2a4ebeddfa3dc034f07558c2f2d6d7f'
+    account_sid = ENV['TWILIO_ACCOUNT_SID']
+    auth_token = ENV['TWILIO_AUTH_TOKEN']
     client = Twilio::REST::Client.new(account_sid, auth_token)
   
     phone_number = self.phone_number
-    unless phone_number.start_with?("+")
-      phone_number = "+1" + phone_number
-    end
-  
-    unless Phonelib.parse(phone_number).valid?
-      # Handle invalid phone number error
-      return
-    end
   
     client.messages.create(
-      from: '+15075163230',
+      from: ENV['TWILIO_PHONE_NUMBER'],
       to: phone_number,
       body: 'Welcome to our application! Your account has been successfully created.'
     )
   end
-  
-  
 end
