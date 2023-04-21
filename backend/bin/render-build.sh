dc8cb07e1b2c2ddc5fd5929c65b1aa891033e495
Moses <ochachamoses443@gmail.com>
2023-04-21 12:15:11 +0300

    deploy using postgres setup

--- backend/bin/render-build.sh
+++ backend/bin/render-build.sh
@@ -3,9 +3,8 @@
 set -o errexit
 
 bundle install
-# bundle exec rake assets:precompile
-# bundle exec rake assets:clean
-bundle exec rails db:drop
+
+bundle exec rails db:drop DISABLE_DATABASE_ENVIRONMENT_CHECK=1
 bundle exec rails db:create
 bundle exec rails db:migrate
 bundle exec rails db:seed
