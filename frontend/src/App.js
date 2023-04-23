3af20a63057a8a75e00387486780530cf306d273
maxwellmuchiri <muchirimaxwel@gmail.com>
2023-04-23 17:34:10 +0300

    Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import React from 'react';
+import Footer from './Components/landing-page/Footer';
 
 function App() {
   return (
     <div className="App">
       <h1>Cash Flow web App UI</h1>
+      <Footer />
     </div>
   );
 }
