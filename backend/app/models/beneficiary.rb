dea16a9c88865ae8f035d587df6453188ab269e5
RoyZiO <nziokinroy@gmail.com>
2023-04-24 11:32:49 +0300

    Added Validations to Models

--- backend/app/models/beneficiary.rb
+++ backend/app/models/beneficiary.rb
@@ -1,4 +1,9 @@
 class Beneficiary < ApplicationRecord
  belongs_to :account
  has_many :transactions
+
+
+    validates :name, presence: true, length: { minimum: 2, maximum: 100 }
+    validates :email, presence: true, length: { maximum: 255 }, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :phone_number, presence: true, numericality: { only_integer: true }, length: { minimum: 8, maximum: 15 }
 end
