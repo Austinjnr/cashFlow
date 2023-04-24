0e55e11a4f34920ff43895aa30db1927454f2677
Austinjnr <Austinobonyo@gmail.com>
2023-04-24 12:46:55 +0300

    transactions

--- frontend/src/Components/userNavbar/Transactions.js
+++ frontend/src/Components/userNavbar/Transactions.js
@@ -1,12 +1,12 @@
-import { useState } from "react";
+// import { useState } from "react";
 
 const Transactions = () => {
-    const [transactions,setTransactions] = useState([
-        { id: 1, transaction_type: "shopping", amount: 36851 },
-        { id: 2, transaction_type: "top_up", amount: 78072  },
-        { id: 3, transaction_type: "top_up", amount: 13019 },
-        { id: 4,transaction_type: "bills", amount: 72186 }
-    ])
+    // const [transactions,setTransactions] = useState([
+    //     { id: 1, transaction_type: "shopping", amount: 36851 },
+    //     { id: 2, transaction_type: "top_up", amount: 78072  },
+    //     { id: 3, transaction_type: "top_up", amount: 13019 },
+    //     { id: 4,transaction_type: "bills", amount: 72186 }
+    // ])
     return ( 
         <section>
             <ol>
