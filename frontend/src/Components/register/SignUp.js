a2f97dc757acad2b834aa0d70fbb697635af3a62
Moses <ochachamoses443@gmail.com>
2023-04-25 15:25:55 +0300

    solved account id

--- frontend/src/Components/register/SignUp.js
+++ frontend/src/Components/register/SignUp.js
@@ -16,7 +16,7 @@ function SignUp(props) {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await fetch("http://localhost:4000/register", {
+    const response = await fetch("https://cashflow-dwee.onrender.com/register", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
