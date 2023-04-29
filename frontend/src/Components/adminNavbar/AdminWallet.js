f3b75157d35c8cd2e6f986e08f78c43652d99599
Austinjnr <Austinobonyo@gmail.com>
2023-04-29 10:21:49 +0300

    dashboard setup

--- frontend/src/Components/adminNavbar/AdminWallet.js
+++ frontend/src/Components/adminNavbar/AdminWallet.js
@@ -1,8 +1,54 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
+import { Line } from 'react-chartjs-2';
+import { 
+    Chart as ChartJS,
+    LineElement,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    Legend 
+} from 'chart.js';
+
+ChartJS.register(
+    LineElement,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    Legend 
+)
 
 const AdminWallet = () => {
   const [transaction, setTransaction] = useState(null);
+
+  const line = {
+        labels: ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'],
+        datasets: [{
+            label: 'weekly transitions',
+            data: [1000, 2000, 300, 4500, 500, 60000, 10500],
+            backgroundColor: [
+                'rgba(137,118,199,0.5)'
+            ],
+            borderColor: 'black',
+            pointBorderColor: [
+                '#8976C7'
+            ],
+            fill: true,
+            tension: 0.4
+        }]
+    }
+
+     const options = {
+        plugins: {
+            legend: true
+        },
+        scales: {
+            y : {
+                // min: 3,
+                // max: 6
+            }
+        }
+    }
 
   useEffect(() => {
     fetch("https://cashflow-1rf2.onrender.com/transactions")
@@ -22,7 +68,7 @@ const AdminWallet = () => {
         <div className="card mb-3">
           <div className="card-body">
             <h5 className="text-center">This Week Transactions</h5>
-            <p>LineGraph</p>
+             <Line data={line} options={options}/>
           </div>
         </div>
       </div>
--- frontend/src/Components/adminNavbar/Dashboard.js
+++ frontend/src/Components/adminNavbar/Dashboard.js
@@ -1,4 +1,73 @@
+import React from "react";
+import {  Line, Bar } from "react-chartjs-2";
+import { 
+    Chart as ChartJS,
+    LineElement,
+    BarElement,
+    Tooltip,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    Legend 
+} from 'chart.js';
+
+ChartJS.register(
+    LineElement,
+    BarElement,
+    Tooltip,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    Legend 
+)
 const Dashboard = () => {
+
+    const line = {
+        labels: ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'],
+        datasets: [{
+            label: 'users gained',
+            data: [10, 20 ,30, 40, 50, 60, 10],
+            backgroundColor: [
+                'rgba(137,118,199,0.5)'
+            ],
+            borderColor: 'black',
+            pointBorderColor: [
+                '#8976C7'
+            ],
+            fill: true,
+            tension: 0.4
+        }]
+    }
+
+    const bar = {
+        labels: ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        datasets: [{
+            label: 'Transactions',
+            data: [0, 0 ,0, 4000 ],
+            backgroundColor: [
+                '#8976C7'
+            ],
+            borderColor: 'black',
+            borderwidth: 1
+        }]
+    }
+
+    const options = {
+        plugins: {
+            legend: true
+        },
+        scales: {
+            y : {
+                // min: 3,
+                // max: 6
+            }
+        }
+    }
+
+    const Baroptions ={
+
+    }
+
   return (
     <section className="overview mt-5">
       <h1 className="text-center">WELCOME</h1>
@@ -7,7 +76,10 @@ const Dashboard = () => {
         <div className="col">
           <div className="card">
             <div className="card-body">
-              <h5 className="text-center">Total Gained Users</h5>
+              <h5 className="text-center">Total Weekly Gained Users</h5>
+              <div>
+              <Line data={line} options={options}/>
+              </div>
             </div>
           </div>
         </div>
@@ -15,7 +87,7 @@ const Dashboard = () => {
           <div className="card">
             <div className="card-body">
               <h5 className="text-center">Total Monthy Transactions</h5>
-              <p>BarGraph</p>
+              <Bar data={bar} options={Baroptions}/>
             </div>
           </div>
         </div>
