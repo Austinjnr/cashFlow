b3602daf0cfa49a278ad3c81c9933b873e1fa18f
Moses <ochachamoses443@gmail.com>
2023-04-30 18:54:17 +0300

    Complited account setup

--- backend/bin/render-build.sh
+++ backend/bin/render-build.sh
@@ -7,4 +7,4 @@ bundle install
 # bundle exec rails db:drop DISABLE_DATABASE_ENVIRONMENT_CHECK=1
 bundle exec rails db:create
 bundle exec rails db:migrate
-# bundle exec rails db:seed
+bundle exec rails db:seed
