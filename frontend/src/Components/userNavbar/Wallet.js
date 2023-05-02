4bc78c77781294e652819e22221e5911a7a3d10a
Moses <ochachamoses443@gmail.com>
2023-05-02 12:35:25 +0300

    Complited account setup

--- frontend/src/Components/userNavbar/Wallet.js
+++ frontend/src/Components/userNavbar/Wallet.js
@@ -1,11 +1,12 @@
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
 const Wallet = ({ userId }) => {
   const [details, setDetails] = useState("");
-  const [transaction, setTransactions] = useState('');
-
+  const [transaction, setTransactions] = useState("");
+  const history = useHistory();
+  
   useEffect(() => {
     axios
       .get(`https://cashflow-1rf2.onrender.com/userprofile/${userId}`)
@@ -15,11 +16,11 @@ const Wallet = ({ userId }) => {
             return (data = data.wallet.balance);
           })
         );
-        setTransactions(res.data.map((data)=>{
-          return (
-            data.transactions
-          )
-        }))
+        setTransactions(
+          res.data.map((data) => {
+            return data.transactions;
+          })
+        );
       });
   }, [userId]);
 
@@ -43,9 +44,14 @@ const Wallet = ({ userId }) => {
               width: "50%",
             }}
           >
-            <Link to="/send">
-              <button>Send</button>
-            </Link>
+            <button
+              onClick={() => {
+                history.push("/send");
+              }}
+            >
+              Send
+            </button>
+
             <Link to="/top-up">
               <button>Deposit</button>
             </Link>
@@ -63,28 +69,34 @@ const Wallet = ({ userId }) => {
         <div className="col-md-4">
           <h3>Overview</h3>
           {transaction.length > 0 && (
-  <table class="table">
-    <thead>
-      <tr>
-        <th scope="col">Date</th>
-        <th scope="col">Transaction type</th>
-        <th scope="col">Amount</th>
-        <th scope="col">transaction cost</th>
-      </tr>
-    </thead>
-    <tbody>
-      {transaction[0].map((transact) => (
-        <tr key={transact.id}>
-          <th scope="row">{new Date(transact.created_at).toLocaleString('en-us', { weekday: 'long', hour: 'numeric', minute:'numeric', hour12: true })}</th>
-          <td>{transact.transaction_type}</td>
-          <td>{transact.amount} Ksh</td>
-          <td>{transact.transaction_fee} Ksh</td>
-        </tr>
-      ))}
-    </tbody>
-  </table>
-)}
-
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">Date</th>
+                  <th scope="col">Transaction type</th>
+                  <th scope="col">Amount</th>
+                  <th scope="col">transaction cost</th>
+                </tr>
+              </thead>
+              <tbody>
+                {transaction[0].map((transact) => (
+                  <tr key={transact.id}>
+                    <th scope="row">
+                      {new Date(transact.created_at).toLocaleString("en-us", {
+                        weekday: "long",
+                        hour: "numeric",
+                        minute: "numeric",
+                        hour12: true,
+                      })}
+                    </th>
+                    <td>{transact.transaction_type}</td>
+                    <td>{transact.amount} Ksh</td>
+                    <td>{transact.transaction_fee} Ksh</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )}
         </div>
       </div>
     </section>
