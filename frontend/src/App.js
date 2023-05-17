5b3edb86ca5732673c283af6dafb0e55919a2443
Moses <ochachamoses443@gmail.com>
2023-05-17 11:34:07 +0300

    updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import "./App.css"
 import {
   BrowserRouter as Router,
   Switch,
@@ -28,7 +29,7 @@ import AdminWallet from "./Components/adminNavbar/AdminWallet";
 import AdminHome from "./Components/adminNavbar/AdminHome";
 import UserDetails from "./Components/adminNavbar/UserDetails";
 import UpdateUser from "./Components/adminNavbar/UpdateUser";
-import NotFound from "./Components/NotFound";
+// import NotFound from "./Components/NotFound";
 import Blogs from "./Components/landing-page/Blogs";
 import CustomerCare from "./Components/landing-page/CustomerCare";
 import Dashboard from "./Components/adminNavbar/Dashboard";
@@ -77,69 +78,76 @@ export default function App() {
   return (
     <div data-testid="Navigation-1">
       <Router>
-        {navbar}
+        <div className="Navbars"> {navbar}</div>
+
         <Switch>
-          {/* landing page  <LandingNavbar />  */}
-          <Route exact path="/" component={CashFlowLandingpg} />
-          <Route exact path="/fees" component={Fees} />
-          <Route exact path="/rates" component={Rates} />
-          <Route exact path="/blogs" component={Blogs} />
-          <Route exact path="/contact-us" component={Contact} />
-          <Route exact path="/customer-care" component={CustomerCare} />
-          <Route exact path="/login" component={Authentication} />
-          <Route exact path="/reset-password" component={Reset} />
-          <Route exact path="/sign-up" component={SignUp} />
-          <Route exact path="/contract" component={Contract} />
-          <Route exact path="/questions" component={Questions} />
-          <Route exact path='/welcome' component={Home} />
+          <div className="body">
+            {/* landing page  <LandingNavbar />  */}
+            <Route exact path="/" component={CashFlowLandingpg} />
+            <Route exact path="/fees" component={Fees} />
+            <Route exact path="/rates" component={Rates} />
+            <Route exact path="/blogs" component={Blogs} />
+            <Route exact path="/contact-us" component={Contact} />
+            <Route exact path="/customer-care" component={CustomerCare} />
+            {/* <Route exact path="/login" component={Authentication} /> */}
+            <Route exact path="/reset-password" component={Reset} />
+            <Route exact path="/sign-up" component={SignUp} />
+            <Route exact path="/contract" component={Contract} />
+            <Route exact path="/questions" component={Questions} />
+            <Route exact path="/welcome" component={Home} />
 
-          {/* NAvbar */}
-          <Route
-            exact
-            path="/user-wallet"
-            render={() => <Wallet AccountId={account} 
-            userId={session}
-            />}
-          />
-          <Route exact path="/send" 
-         render={() => <Send AccountId={account} />}
-           />
-          <Route
-            exact
-            path="/top-up"
-            render={() => <Deposit AccountId={account} />}
-          />
-          <Route
-            exact
-            path="/profile-setup"
-            render={() => <ProfileSetup userId={session}  />}
-          />
-          <Route
-            exact
-            path="/user-profile"
-            render={() => <Profile userId={session} />}
-          />
-          <Router
-            exact
-            path="/user-transactions"
-          />
-          <Route exact path="/new-beneficary" 
-          render={() => <AddBeneficiary AccountId={account} />}/>
-          <Route exact 
-          path="/beneficiaries" 
-          render={() => <Beneficiaries userId={session}/>}/> 
-          <Route exact path="/user-transaction" 
-           render={() => <Transaction userId={session} />}
-           />
+            {/* NAvbar */}
+            <Route
+              exact
+              path="/user-wallet"
+              render={() => <Wallet AccountId={account} userId={session} />}
+            />
+            <Route
+              exact
+              path="/send"
+              render={() => <Send AccountId={account} />}
+            />
+            <Route
+              exact
+              path="/top-up"
+              render={() => <Deposit AccountId={account} />}
+            />
+            <Route
+              exact
+              path="/profile-setup"
+              render={() => <ProfileSetup userId={session} />}
+            />
+            <Route
+              exact
+              path="/user-profile"
+              render={() => <Profile userId={session} />}
+            />
+            <Router exact path="/user-transactions" />
+            <Route
+              exact
+              path="/new-beneficary"
+              render={() => <AddBeneficiary AccountId={account} />}
+            />
+            <Route
+              exact
+              path="/beneficiaries"
+              render={() => <Beneficiaries userId={session} />}
+            />
+            <Route
+              exact
+              path="/user-transaction"
+              render={() => <Transaction userId={session} />}
+            />
 
-          {/* <AdminNav />    */}
-          <Route exact path='/dashboard' component={Dashboard} />
-          <Route exact path="/all-users" component={AdminHome} />
-          <Route exact path="/user/:id" component={UserDetails} />
-          <Route exact path="/update-user" component={UpdateUser} />
-          <Route exact path="/company-wallet" component={AdminWallet} />
-          <Route exact path="/login" component={Authentication} />
-          <Route exact path="*" component={NotFound} />
+            {/* <AdminNav />    */}
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/all-users" component={AdminHome} />
+            <Route exact path="/user/:id" component={UserDetails} />
+            <Route exact path="/update-user" component={UpdateUser} />
+            <Route exact path="/company-wallet" component={AdminWallet} />
+            <Route exact path="/login" component={Authentication} />
+            {/* <Route exact path="*" component={NotFound} /> */}
+          </div>
         </Switch>
       </Router>
     </div>
