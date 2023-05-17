ac76f5551066ba322bed35293e983d25c5b0276b
Moses <ochachamoses443@gmail.com>
2023-05-17 12:30:37 +0300

    updated refresh action

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -30,7 +30,9 @@ module Backend
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+  
+    config.time_zone = "Nairobi"
+
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Only loads a smaller set of middleware suitable for API only apps.
